252bbd787581c455d71d32ec52cd56f2a86b5a8f
fragfutter <fragfutter@gmail.com>
2022-01-29 11:36:13 +0100

    CH: doc

--- common_flag/common_flag.py
+++ common_flag/common_flag.py
@@ -3,34 +3,42 @@ from itertools import chain
 import click
 
 """
-common flags
+## common flags
 
 Have an option for a CommandGroup that can be specified
 anywhere on it's subcommands.
 
 clicks default model is
 
-   cli --debug cmd1 --cmd1-flag1
-   cli --debug cmd2 --cmd2-flag1
+    cli --debug cmd1 --cmd1-flag1
+    cli --debug cmd2 --cmd2-flag1
 
 or one would need to add the debug flag both subcommands
 
 It would be nice to specify the debug flag anywhere
 so both these calls would work
 
-   cli cmd1 --debug --cmd1-flag1
-   cli --debug cmd1 --cmd1-flag1
+    cli cmd1 --debug --cmd1-flag1
+    cli --debug cmd1 --cmd1-flag1
 
 This contradicts clicks design decission. But for common
 flags like debug, verbose, quiet it is realy nice.
 
 See also
 https://github.com/pallets/click/issues/108
+
+Example:
+```
+@click.group(cls=CommonGroup)
+@click.option('--debug', is_flag=True, cls=CommonFlag)
+def cli(debug):
+    pass
+
+@cli.command()
+def cmd1():
+    pass
+```
 """
-
-DEBUG = None
-QUIET = None
-
 
 class CommonFlag(click.Option):
     def __init__(self, *args, **kwargs):
